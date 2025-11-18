6236beaa0a271aa7a316572f251ba028d01573f3
gowriakr <prathita.gowri@dynpro.in>
2025-11-18 15:48:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,15 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo "[INFO] Stopping and removing old containers..."
 docker compose down
 
+echo "[INFO] Removing old images..."
+docker rmi -f dynprobangalore/dynviz-v1:frontend || true
+docker rmi -f dynprobangalore/dynviz-v1:backend || true
+
 echo "[INFO] Pulling latest images..."
 docker compose pull
 
 echo "[INFO] Recreating containers with latest images..."
-docker compose up -d
+docker compose up -d --remove-orphans
 
 echo "[INFO] Cleaning unused images..."
 docker image prune -f
