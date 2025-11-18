18bb87b20cd736577cb77a5c1744de498ff4b333
gowriakr <prathita.gowri@dynpro.in>
2025-11-18 14:58:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,13 @@ set -e
 echo "[INFO] Logging in to Docker Hub..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+echo "[INFO] Stopping and removing old containers..."
+docker compose down
+
 echo "[INFO] Pulling latest images..."
 docker compose pull
 
-echo "[INFO] Restarting containers..."
+echo "[INFO] Recreating containers with latest images..."
 docker compose up -d
 
 echo "[INFO] Cleaning unused images..."
